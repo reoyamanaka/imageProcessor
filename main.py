40a01cf36ea374f4a6d841912d197ac6b12180f8
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-18 02:28:06 -0500

    finished classes for app, frame and panel

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import wx
+
+class MyApp(wx.App):
+	def __init__(self):
+		super().__init__(clearSigInt = True)
+		frame = MyFrame()
+		frame.Show()
+
+class MyFrame(wx.Frame):
+	def __init__(self):
+		super().__init__(parent = None, title = "Image Processor", pos = (20, 20), size = (700, 700))
+		self.OnInit()
+
+	def OnInit(self):
+		self.panel = MyPanel(self)
+
+class MyPanel(wx.Panel):
+	def __init__(self, parent):
+		super().__init__(parent = parent)
+
+if __name__ == "__main__":
+	app = MyApp()
+	app.MainLoop()	
