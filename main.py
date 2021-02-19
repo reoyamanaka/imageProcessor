33946ea4b103b082f6a214b87c7a8eb6fe3e415a
Reo Yamanaka <ryamanaka807@gmail.com>
2021-02-18 20:39:29 -0500

    started menubar

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ class MyApp(wx.App):
 		frame = MyFrame()
 		frame.Show()
 
+class FileMenu(wx.Menu):
+	def __init__(self, parentFrame):
+		super().__init__()
+		self.OnInit()
+		self.parentFrame = parentFrame
+
+	def OnInit(self):
+		aboutItem = wx.MenuItem(parentMenu = self, id = wx.ID_ANY, text="&About")
+		self.Append(aboutItem)
+		self.Bind(wx.EVT_MENU, handler = self.onAbout, source = aboutItem)
+	
+	def onAbout(self, event):
+		aboutFrame = AboutFrame()
+		aboutFrame.show()	
+
 class MyFrame(wx.Frame):
 	def __init__(self):
 		super().__init__(parent = None, title = "Image Processor", pos = (20, 20), size = (700, 700))
@@ -13,7 +28,10 @@ class MyFrame(wx.Frame):
 
 	def OnInit(self):
 		self.panel = MyPanel(self)
-
+		menuBar = wx.MenuBar()
+		fileMenu = FileMenu(parentFrame = self)
+		menuBar.Append(fileMenu, '&File')
+		self.SetMenuBar(menuBar)
 class MyPanel(wx.Panel):
 	def __init__(self, parent):
 		super().__init__(parent = parent)
